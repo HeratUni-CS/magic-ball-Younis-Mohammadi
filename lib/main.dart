f6c4d8279121ec55146a8d4511b29dc160f7f8a5
Mohammad Younis <artacodding2023@gmail.com>
2023-06-12 06:34:00 -0700

    all project

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,55 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    const MaterialApp(
-      home: null,
-    ),
-  );
+void main() => runApp(
+      MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          backgroundColor: Colors.black87,
+          appBar: AppBar(
+            title: Center(
+              child: Text('MAGIC 8 BALL'),
+            ),
+            backgroundColor: Colors.black87,
+          ),
+          body: BallPage(),
+        ),
+      ),
+    );
+
+class BallPage extends StatefulWidget {
+  @override
+  State<BallPage> createState() => _BallPageState();
+}
+
+class _BallPageState extends State<BallPage> {
+  int ballAnswer = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          'Ask Me Any Question ?',
+          style: TextStyle(
+            fontSize: 30.0,
+            color: Colors.white,
+          ),
+        ),
+        Expanded(
+          child: TextButton(
+            style: TextButton.styleFrom(
+              foregroundColor: Colors.white,
+            ),
+            onPressed: () {
+              setState(() {
+                ballAnswer = Random().nextInt(5) + 1;
+              });
+            },
+            child: Image.asset('images/ball$ballAnswer.png'),
+          ),
+        ),
+      ],
+    );
+  }
 }
